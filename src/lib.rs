ee562f84a96ab72c227adb180ba041f0c321913e
Frank Jimenez <jjcfrank@gmail.com>
2024-12-21 14:33:22 +0000

    fix: resolve lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,7 @@ mod cursor;
 mod error;
 mod fastpfor;
 mod helpers;
-<<<<<<< HEAD
 mod variable_byte;
-=======
 
 pub use error::{FastPForError, FastPForResult};
 pub use fastpfor::FastPFOR;
->>>>>>> origin/main
