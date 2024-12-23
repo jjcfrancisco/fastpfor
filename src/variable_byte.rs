82dc7bc553945811bbe1bdbbaba24fd9d6693fe9
Frank Jimenez <jjcfrank@gmail.com>
2024-12-23 13:10:15 +0000

    feat: initial uncompress variable byte

--- src/variable_byte.rs
+++ src/variable_byte.rs
@@ -23,29 +23,49 @@ impl VariableByte {
                     // Return early if there is no data to compress
                     return Ok(());
                 }
-                let out_pos_tmp = out_pos.position();
+                let mut out_pos_tmp = out_pos.position();
                 for k in in_pos.position() as i32..(in_pos.position() as i32 + inlength) {
                     let val = input[k as usize] as i64;
                     if val < (1 << 7) {
-                        output.push((val | (1 << 7)) as u8);
+                        // output.push((val | (1 << 7)) as u8);
+                        output[out_pos_tmp as usize] = (val | (1 << 7)) as u8;
+                        out_pos_tmp += 1;
                     } else if val < (1 << 14) {
-                        output.push(extract7bits(0, val));
-                        output.push(extract_7bits_maskless(1, val) | (1 << 7));
+                        output[out_pos_tmp as usize] = extract7bits(0, val) as u8;
+                        out_pos_tmp += 1;
+                        output[out_pos_tmp as usize] =
+                            (extract_7bits_maskless(1, val) | (1 << 7)) as u8;
+                        out_pos_tmp += 1;
                     } else if val < (1 << 21) {
-                        output.push(extract7bits(0, val));
-                        output.push(extract7bits(1, val));
-                        output.push(extract_7bits_maskless(2, val) | (1 << 7));
+                        output[out_pos_tmp as usize] = extract7bits(0, val) as u8;
+                        out_pos_tmp += 1;
+                        output[out_pos_tmp as usize] = extract7bits(1, val) as u8;
+                        out_pos_tmp += 1;
+                        output[out_pos_tmp as usize] =
+                            (extract_7bits_maskless(2, val) | (1 << 7)) as u8;
+                        out_pos_tmp += 1;
                     } else if val < (1 << 28) {
-                        output.push(extract7bits(0, val));
-                        output.push(extract7bits(1, val));
-                        output.push(extract7bits(2, val));
-                        output.push(extract_7bits_maskless(3, val) | (1 << 7));
+                        output[out_pos_tmp as usize] = extract7bits(0, val) as u8;
+                        out_pos_tmp += 1;
+                        output[out_pos_tmp as usize] = extract7bits(1, val) as u8;
+                        out_pos_tmp += 1;
+                        output[out_pos_tmp as usize] = extract7bits(2, val) as u8;
+                        out_pos_tmp += 1;
+                        output[out_pos_tmp as usize] =
+                            (extract_7bits_maskless(3, val) | (1 << 7)) as u8;
+                        out_pos_tmp += 1;
                     } else {
-                        output.push(extract7bits(0, val));
-                        output.push(extract7bits(1, val));
-                        output.push(extract7bits(2, val));
-                        output.push(extract7bits(3, val));
-                        output.push(extract_7bits_maskless(4, val) | (1 << 7));
+                        output[out_pos_tmp as usize] = extract7bits(0, val) as u8;
+                        out_pos_tmp += 1;
+                        output[out_pos_tmp as usize] = extract7bits(1, val) as u8;
+                        out_pos_tmp += 1;
+                        output[out_pos_tmp as usize] = extract7bits(2, val) as u8;
+                        out_pos_tmp += 1;
+                        output[out_pos_tmp as usize] = extract7bits(3, val) as u8;
+                        out_pos_tmp += 1;
+                        output[out_pos_tmp as usize] =
+                            (extract_7bits_maskless(4, val) | (1 << 7)) as u8;
+                        out_pos_tmp += 1;
                     }
                 }
                 out_pos.set_position(out_pos_tmp + inlength as u64);
@@ -104,6 +124,84 @@ impl VariableByte {
 
         FastPForResult::Ok(())
     }
+
+    #[expect(unused_variables)]
+    fn uncompress(
+        &mut self,
+        input: &Vec<i32>,
+        in_pos: &mut Cursor<i32>,
+        inlength: i32,
+        output: &mut Output,
+        out_pos: &mut Cursor<i32>,
+    ) -> FastPForResult<()> {
+        match output {
+            Output::I32(output) => {
+                let mut s = 0;
+                let val = 0 as u8;
+                let mut p = in_pos.position() as i32;
+                let final_p = in_pos.position() as i32 + inlength;
+                let mut tmp_outpos = out_pos.position();
+                let mut shift = 0;
+                let mut v = 0;
+
+                while p < final_p {
+                    let val = input[p as usize];
+                    let c = val >> s;
+                    s += 8;
+                    p += s >> 5;
+                    s = s & 31;
+                    v += (c & 127) << shift;
+                    if (c & 128) == 128 {
+                        output[tmp_outpos as usize] = v;
+                        tmp_outpos += 1;
+                        v = 0;
+                        shift = 0;
+                    } else {
+                        shift += 7;
+                    }
+                }
+                out_pos.set_position(tmp_outpos);
+                in_pos.add(inlength);
+
+                FastPForResult::Ok(())
+            }
+            Output::Byte(output) => {
+                let mut p = in_pos.position() as i32;
+                let final_p = in_pos.position() as i32 + inlength;
+                let mut tmp_outpos = out_pos.position();
+                let mut v = 0;
+                while p < final_p {
+                    v = input[p as usize];
+                    if input[p as usize] < 0 {
+                        p += 1;
+                        continue;
+                    }
+                    v = ((input[p as usize + 1]) << 7) | v;
+                    if input[p as usize + 1] < 0 {
+                        p += 2;
+                        continue;
+                    }
+                    v = ((input[p as usize + 2]) << 14) | v;
+                    if input[p as usize + 2] < 0 {
+                        p += 3;
+                        continue;
+                    }
+                    v = ((input[p as usize + 3]) << 21) | v;
+                    if input[p as usize + 3] < 0 {
+                        p += 4;
+                        continue;
+                    }
+                    v = ((input[p as usize + 4]) << 28) | v;
+                    p += 5;
+                    output[tmp_outpos as usize] = v as u8;
+                    tmp_outpos += 1;
+                }
+                out_pos.set_position(tmp_outpos);
+                in_pos.add(inlength);
+                FastPForResult::Ok(())
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -114,7 +212,7 @@ mod tests {
     fn test_empty_array() {
         // All empty
         let mut codec = VariableByte;
-        let mut output = Output::I32(Vec::new());
+        let mut output = Output::I32(vec![]);
         codec
             .compress(
                 &vec![],
@@ -125,5 +223,34 @@ mod tests {
             )
             .expect("Failed to compress");
         assert_eq!(output, Output::I32(vec![]));
+        // Needs uncompressing
+    }
+
+    #[test]
+    fn saul_test() {
+        let mut codec = VariableByte;
+
+        for x in 0..50 {
+            let a = vec![2, 3, 4, 5];
+            let b = vec![0; 90];
+            let mut a_offset = Cursor::new(0);
+            let mut b_offset = Cursor::new(x);
+            let mut output = Output::I32(b);
+            // println!("output before: {:?}", output);
+            codec
+                .compress(
+                    &a,
+                    &mut a_offset,
+                    a.len() as i32,
+                    &mut output,
+                    &mut b_offset,
+                )
+                .expect("Failed to compress");
+            // println!("output after: {:?}", output);
+            #[expect(unused_variables)]
+            let len = b_offset.position() as i32 - x;
+            b_offset.set_position(x as u64);
+            println!("b_offset: {:?}", b_offset);
+        }
     }
 }
