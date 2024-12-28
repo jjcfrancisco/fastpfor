994cd911df238c09068b2069efd6c7b808c2e968
Frank Jimenez <jjcfrank@gmail.com>
2024-12-28 22:48:41 +0000

    feat: enum codec and impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,58 @@
 mod bitpacking;
 mod bytebuffer;
+mod composition;
+mod compressor;
 mod cursor;
 mod error;
 mod fastpfor;
 mod helpers;
 mod variable_byte;
 
+use std::io::Cursor;
+
 pub use error::{FastPForError, FastPForResult};
 pub use fastpfor::{FastPFOR, BLOCK_SIZE_128, BLOCK_SIZE_256, DEFAULT_PAGE_SIZE};
 pub use variable_byte::VariableByte;
 
-#[derive(Debug, PartialEq)]
-pub enum Output {
-    Byte(Vec<u8>),
-    I32(Vec<i32>),
+pub enum Codec {
+    FastPFor(FastPFOR),
+    VariableByte(VariableByte),
+}
+
+impl Codec {
+    fn compress(
+        &mut self,
+        input: &[i32],
+        input_length: i32,
+        input_offset: &mut Cursor<i32>,
+        output: &mut [i32],
+        output_offset: &mut Cursor<i32>,
+    ) -> FastPForResult<()> {
+        match self {
+            Codec::FastPFor(fastpfor) => {
+                fastpfor.compress(input, input_length, input_offset, output, output_offset)
+            }
+            Codec::VariableByte(vb) => {
+                vb.compress(input, input_length, input_offset, output, output_offset)
+            }
+        }
+    }
+
+    fn uncompress(
+        &mut self,
+        input: &[i32],
+        input_length: i32,
+        input_offset: &mut Cursor<i32>,
+        output: &mut [i32],
+        output_offset: &mut Cursor<i32>,
+    ) -> FastPForResult<()> {
+        match self {
+            Codec::FastPFor(fastpfor) => {
+                fastpfor.uncompress(input, input_length, input_offset, output, output_offset)
+            }
+            Codec::VariableByte(vb) => {
+                vb.uncompress(input, input_length, input_offset, output, output_offset)
+            }
+        }
+    }
 }
