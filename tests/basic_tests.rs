af5a0d6fba5a781b856b7e36686d432dbf9d38e2
Frank Jimenez <jjcfrank@gmail.com>
2024-12-28 00:30:52 +0000

    fix: improved integration test for variable byte

--- tests/basic_tests.rs
+++ tests/basic_tests.rs
@@ -1,44 +1,46 @@
+use fastpfor::{Codec, Compressor};
 use std::io::Cursor;
-
-use fastpfor::Output;
 
 mod common;
 
 fn saul_test() {
-    let codecs = common::get_codecs();
+    let codecs = common::get_integer_codecs();
 
-    for mut codec in codecs {
+    for codec in codecs {
+        // unwrap the codec
+        let mut codec = match codec {
+            Codec::VariableByte(codec) => codec,
+        };
         for x in 0..50 {
-            let a = vec![2, 3, 4, 5];
-            let mut b = Output::I32(vec![0; 90]);
-            let mut c = Output::I32(vec![0; a.len()]);
-            let mut a_offset = Cursor::new(0);
-            let mut b_offset = Cursor::new(0);
-            b_offset.set_position(x as u64);
+            let input = vec![2, 3, 4, 5];
+            let mut output: Vec<i32> = vec![0; 90];
+            let mut answer = output.clone();
+            let mut input_offset = Cursor::new(0);
+            let mut output_offset = Cursor::new(0);
+            output_offset.set_position(x as u64);
             codec
-                .compress(&a, &mut a_offset, a.len() as i32, &mut b, &mut b_offset)
+                .compress(
+                    &input,
+                    &mut input_offset,
+                    input.len() as i32,
+                    &mut output,
+                    &mut output_offset,
+                )
                 .expect("Failed to compress");
-            let comp = {
-                match b {
-                    Output::I32(output) => output,
-                    _ => panic!("Output is not I32"),
-                }
-            };
-            let len = b_offset.position() as i32 - x;
-            b_offset.set_position(x as u64);
+            let len = output_offset.position() as i32 - x;
+            output_offset.set_position(x as u64);
 
-            let mut c_offset = Cursor::new(0);
             codec
-                .uncompress(&comp, &mut b_offset, len, &mut c, &mut c_offset)
+                .uncompress(
+                    &output,
+                    &mut input_offset,
+                    len,
+                    &mut answer,
+                    &mut Cursor::new(0),
+                )
                 .expect("Failed to uncompress");
-            let uncomp = {
-                match c {
-                    Output::I32(output) => output,
-                    _ => panic!("Output is not I32"),
-                }
-            };
 
-            assert_eq!(a, uncomp);
+            assert_eq!(input, answer);
         }
     }
 }
@@ -50,12 +52,15 @@ fn test_varying_length() {
     for k in 0..n {
         data[k] = k as i32;
     }
-    let codecs = common::get_codecs();
-    for mut codec in codecs {
+    let codecs = common::get_integer_codecs();
+    for codec in codecs {
+        let mut codec = match codec {
+            Codec::VariableByte(codec) => codec,
+        };
         for l in 1..128 {
             let mut data_copy = data.clone();
             data_copy.resize(l, 0);
-            let mut output_compress = Output::I32(vec![0; data_copy.len() * 4]);
+            let mut output_compress = vec![0; data_copy.len() * 4];
             codec
                 .compress(
                     &data_copy,
@@ -65,30 +70,18 @@ fn test_varying_length() {
                     &mut Cursor::new(0),
                 )
                 .expect("Failed to compress");
-            let comp = {
-                match output_compress {
-                    Output::I32(output) => output,
-                    _ => panic!("Output is not I32"),
-                }
-            };
-            let mut answer = Output::I32(vec![0; l + 1024]);
+            let mut answer = vec![0; l + 1024];
             codec
                 .uncompress(
-                    &comp,
+                    &output_compress,
                     &mut Cursor::new(0),
-                    comp.len() as i32,
+                    output_compress.len() as i32,
                     &mut answer,
                     &mut Cursor::new(0),
                 )
                 .expect("Failed to uncompress");
-            let uncomp = {
-                match answer {
-                    Output::I32(output) => output,
-                    _ => panic!("Output is not I32"),
-                }
-            };
             for k in 0..l {
-                assert_eq!(uncomp[k], data[k]);
+                assert_eq!(answer[k], data[k]);
             }
         }
     }
@@ -99,11 +92,14 @@ fn test_varying_length_two() {
     let n = 128;
     let mut data = vec![0; n];
     data[127] = -1;
-    let codecs = common::get_codecs();
-    for mut codec in codecs {
+    let codecs = common::get_integer_codecs();
+    for codec in codecs {
+        let mut codec = match codec {
+            Codec::VariableByte(codec) => codec,
+        };
         for l in 1..128 {
             let mut data_copy = data.clone();
-            let mut output_compress = Output::I32(vec![0; data_copy.len() * 4]);
+            let mut output_compress = vec![0; data_copy.len() * 4];
             data_copy.resize(l, 0);
             codec
                 .compress(
@@ -114,30 +110,18 @@ fn test_varying_length_two() {
                     &mut Cursor::new(0),
                 )
                 .expect("Failed to compress");
-            let comp = {
-                match output_compress {
-                    Output::I32(ref output) => output,
-                    _ => panic!("Output is not I32"),
-                }
-            };
-            let mut answer = Output::I32(vec![0; data_copy.len() + 1024]);
+            let mut answer = vec![0; data_copy.len() + 1024];
             codec
                 .uncompress(
-                    comp,
+                    &output_compress,
                     &mut Cursor::new(0),
                     128,
                     &mut answer,
                     &mut Cursor::new(0),
                 )
                 .expect("Failed to uncompress");
-            let uncomp = {
-                match answer {
-                    Output::I32(output) => output,
-                    _ => panic!("Output is not I32"),
-                }
-            };
             for k in 1..l {
-                if uncomp[k] != data[k] {
+                if answer[k] != data[k] {
                     panic!("bug");
                 }
             }
--- tests/common.rs
+++ tests/common.rs
@@ -1,61 +1,6 @@
-use std::io::Cursor;
+use fastpfor::{VariableByte, Codec};
 
-use fastpfor::{FastPFOR, FastPForResult, Output, VariableByte};
-
-#[derive(Debug)]
-pub enum Codec {
-    FastPFOR(FastPFOR),
-    VariableByte(VariableByte),
-}
-
-impl Codec {
-    pub fn compress(
-        &mut self,
-        input: &Vec<i32>,
-        input_offset: &mut Cursor<i32>,
-        input_length: i32,
-        output: &mut Output,
-        output_offset: &mut Cursor<i32>,
-    ) -> FastPForResult<()> {
-        match self {
-            Codec::FastPFOR(codec) => {
-                let outbuf = match output {
-                    Output::I32(o) => o,
-                    _ => panic!("Output is not I32"),
-                };
-                codec.compress(input, input_offset, input_length, outbuf, output_offset)
-            }
-            Codec::VariableByte(codec) => {
-                codec.compress(input, input_offset, input_length, output, output_offset)
-            }
-        }
-    }
-
-    pub fn uncompress(
-        &mut self,
-        input: &Vec<i32>,
-        input_offset: &mut Cursor<i32>,
-        input_length: i32,
-        output: &mut Output,
-        output_offset: &mut Cursor<i32>,
-    ) -> FastPForResult<()> {
-        match self {
-            Codec::FastPFOR(codec) => {
-                let outbuf = match output {
-                    Output::I32(o) => o,
-                    _ => panic!("Output is not I32"),
-                };
-                codec.uncompress(input, input_offset, input_length, outbuf, output_offset)
-            }
-            Codec::VariableByte(codec) => {
-                codec.uncompress(input, input_offset, input_length, output, output_offset)
-            }
-        }
-    }
-}
-
-pub fn get_codecs() -> Vec<Codec> {
+pub fn get_integer_codecs() -> Vec<Codec> {
     let variablebyte = VariableByte;
-
     vec![Codec::VariableByte(variablebyte)]
 }
