a996dc8bf0eaf21d6c03ac0b26ac80637c3395b5
Frank Jimenez <jjcfrank@gmail.com>
2024-12-29 07:11:01 +0000

    fix: integration tests adapted for composition

--- tests/common.rs
+++ tests/common.rs
@@ -1,61 +1,14 @@
-use std::io::Cursor;
-
-use fastpfor::{FastPFOR, FastPForResult, Output, VariableByte};
-
-#[derive(Debug)]
-pub enum Codec {
-    FastPFOR(FastPFOR),
-    VariableByte(VariableByte),
-}
-
-impl Codec {
-    pub fn compress(
-        &mut self,
-        input: &Vec<i32>,
-        input_offset: &mut Cursor<i32>,
-        input_length: i32,
-        output: &mut Output,
-        output_offset: &mut Cursor<i32>,
-    ) -> FastPForResult<()> {
-        match self {
-            Codec::FastPFOR(codec) => {
-                let outbuf = match output {
-                    Output::I32(o) => o,
-                    _ => panic!("Output is not I32"),
-                };
-                codec.compress(input, input_offset, input_length, outbuf, output_offset)
-            }
-            Codec::VariableByte(codec) => {
-                codec.compress(input, input_offset, input_length, output, output_offset)
-            }
-        }
-    }
-
-    pub fn uncompress(
-        &mut self,
-        input: &Vec<i32>,
-        input_offset: &mut Cursor<i32>,
-        input_length: i32,
-        output: &mut Output,
-        output_offset: &mut Cursor<i32>,
-    ) -> FastPForResult<()> {
-        match self {
-            Codec::FastPFOR(codec) => {
-                let outbuf = match output {
-                    Output::I32(o) => o,
-                    _ => panic!("Output is not I32"),
-                };
-                codec.uncompress(input, input_offset, input_length, outbuf, output_offset)
-            }
-            Codec::VariableByte(codec) => {
-                codec.uncompress(input, input_offset, input_length, output, output_offset)
-            }
-        }
-    }
-}
+use fastpfor::{Codec, Composition, FastPFOR, VariableByte, BLOCK_SIZE_256, DEFAULT_PAGE_SIZE};
 
 pub fn get_codecs() -> Vec<Codec> {
     let variablebyte = VariableByte;
 
-    vec![Codec::VariableByte(variablebyte)]
+    vec![
+        Codec::VariableByte(variablebyte),
+        Codec::FastPFor(FastPFOR::new(DEFAULT_PAGE_SIZE, BLOCK_SIZE_256)),
+        Codec::Composition(Box::new(Composition::new(
+            Codec::FastPFor(FastPFOR::new(DEFAULT_PAGE_SIZE, BLOCK_SIZE_256)),
+            Codec::VariableByte(VariableByte),
+        ))),
+    ]
 }
