3dc934faf61db74d02af9919fbb1f4692f0677cf
Frank Jimenez <jjcfrank@gmail.com>
2024-12-30 08:17:11 +0000

    feat: integration tests initial

--- tests/common.rs
+++ tests/common.rs
@@ -1,61 +1,53 @@
+use fastpfor::{Composition, FastPFOR, FastPForResult, VariableByte, Compressor};
 use std::io::Cursor;
 
-use fastpfor::{FastPFOR, FastPForResult, Output, VariableByte};
-
-#[derive(Debug)]
 pub enum Codec {
-    FastPFOR(FastPFOR),
     VariableByte(VariableByte),
+    Composition(Composition),
 }
 
 impl Codec {
     pub fn compress(
         &mut self,
-        input: &Vec<i32>,
-        input_offset: &mut Cursor<i32>,
+        input: &[i32],
         input_length: i32,
-        output: &mut Output,
+        input_offset: &mut Cursor<i32>,
+        output: &mut [i32],
         output_offset: &mut Cursor<i32>,
     ) -> FastPForResult<()> {
         match self {
-            Codec::FastPFOR(codec) => {
-                let outbuf = match output {
-                    Output::I32(o) => o,
-                    _ => panic!("Output is not I32"),
-                };
-                codec.compress(input, input_offset, input_length, outbuf, output_offset)
+            Codec::VariableByte(vb) => {
+                vb.compress(input, input_length, input_offset, output, output_offset)
             }
-            Codec::VariableByte(codec) => {
-                codec.compress(input, input_offset, input_length, output, output_offset)
+            Codec::Composition(comp) => {
+                comp.compress(input, input_length, input_offset, output, output_offset)
             }
         }
     }
 
     pub fn uncompress(
         &mut self,
-        input: &Vec<i32>,
-        input_offset: &mut Cursor<i32>,
+        input: &[i32],
         input_length: i32,
-        output: &mut Output,
+        input_offset: &mut Cursor<i32>,
+        output: &mut [i32],
         output_offset: &mut Cursor<i32>,
     ) -> FastPForResult<()> {
         match self {
-            Codec::FastPFOR(codec) => {
-                let outbuf = match output {
-                    Output::I32(o) => o,
-                    _ => panic!("Output is not I32"),
-                };
-                codec.uncompress(input, input_offset, input_length, outbuf, output_offset)
+            Codec::VariableByte(vb) => {
+                vb.uncompress(input, input_length, input_offset, output, output_offset)
             }
-            Codec::VariableByte(codec) => {
-                codec.uncompress(input, input_offset, input_length, output, output_offset)
+            Codec::Composition(comp) => {
+                comp.uncompress(input, input_length, input_offset, output, output_offset)
             }
         }
     }
 }
 
 pub fn get_codecs() -> Vec<Codec> {
-    let variablebyte = VariableByte;
-
-    vec![Codec::VariableByte(variablebyte)]
+    vec![
+        Codec::VariableByte(VariableByte::new()),
+        Codec::Composition(Composition::new(FastPFOR::default(), VariableByte::new())),
+    ]
 }
+
