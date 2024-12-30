e4a4ec38daa4621825e2e18ca54318871e97b951
Frank Jimenez <jjcfrank@gmail.com>
2024-12-30 08:17:00 +0000

    feat: update with new changes

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,15 @@ mod error;
 mod fastpfor;
 mod helpers;
 mod variable_byte;
+mod compressor;
+mod composition;
+mod integer_codec;
 
 pub use error::{FastPForError, FastPForResult};
 pub use fastpfor::{FastPFOR, BLOCK_SIZE_128, BLOCK_SIZE_256, DEFAULT_PAGE_SIZE};
 pub use variable_byte::VariableByte;
+pub use composition::Composition;
+pub use compressor::Compressor;
 
 #[derive(Debug, PartialEq)]
 pub enum Output {
