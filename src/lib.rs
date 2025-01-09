af279231e08d1d8fa5da12b596d59bf7ee8eb81d
Frank Jimenez <jjcfrank@gmail.com>
2025-01-09 19:09:07 +0000

    feat: update new structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,15 @@
-mod bitpacking;
-mod bytebuffer;
-mod composition;
-mod compressor;
+pub mod bytebuffer;
 mod cursor;
 mod error;
-mod fastpfor;
-mod helpers;
-mod integer_codec;
-mod variable_byte;
+mod integer_compression;
 
-pub use composition::Composition;
-pub use compressor::Compressor;
 pub use error::{FastPForError, FastPForResult};
-pub use fastpfor::{FastPFOR, BLOCK_SIZE_128, BLOCK_SIZE_256, DEFAULT_PAGE_SIZE};
-pub use variable_byte::VariableByte;
+pub use integer_compression::composition::Composition;
+pub use integer_compression::compressors::Integer;
+pub use integer_compression::fastpfor::{
+    FastPFOR, BLOCK_SIZE_128, BLOCK_SIZE_256, DEFAULT_PAGE_SIZE,
+};
+pub use integer_compression::variable_byte::VariableByte;
 
 #[derive(Debug, PartialEq)]
 pub enum Output {
